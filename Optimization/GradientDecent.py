dd76a03252ffd2ecb5eb53329576ce8088f26f66
baixiaohub <baixiaohub@gmail.com>
2024-06-30 00:50:52 +0800

    add GradientDescent sample.

---
+++ Optimization/GradientDecent.py
@@ -0,0 +1,22 @@
+from math import fabs
+
+
+def f(x):
+    return x*x + 3*x - 1
+
+def df(x):
+    return 2*x + 3
+
+
+def gradient_decent(f, df, x0, k):
+    x = x0
+    epsilon = 1e-4
+    max_iter = 100
+    while fabs(df(x)) >= epsilon and max_iter > 0:
+        x = x - k*df(x)
+        max_iter -= 1
+    return x, f(x)
+
+if __name__ == '__main__':
+    x, fx = gradient_decent(f, df, 300, 0.1)
+    print('min f(x) at x:{}, f(x):{}'.format(x, fx))
